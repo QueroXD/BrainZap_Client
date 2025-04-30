762705065d204f01556e6ac93f4adc3b716837c1
mbaeta8 <mbaeta8@gmail.com>
2025-04-30 16:38:05 +0200

    Clases

---
+++ BrainZap_Client/CLASSES/ClJugador.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainZap_Client.CLASSES
+{
+    internal class ClJugador
+    {
+        //Representa al jugador local: nickname, puntuación.
+    }
+}
---
+++ BrainZap_Client/CLASSES/ClUIManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainZap_Client.CLASSES
+{
+    internal class ClUIManager
+    {
+        //Muestra preguntas y resultados, recoge input del usuario.
+    }
+}
